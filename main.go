da8d766b3b456a7149f14e257b193ab2daaa5283
Hope Kalitera <hopekalitera@gmail.com>
2024-05-09 19:51:07 +0200

    chore: websocket impl

--- main.go
+++ main.go
@@ -1,13 +1,48 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"io"
 
+	//"github.com/gorilla/websocket"
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func newServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+func (s *Server) reedLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error : ", err)
+			continue
+		}
+
+		msg := buf[:n]
+		fmt.Println(string(msg))
+
+		ws.Write([]byte("Message received successfully :) "))
+	}
+}
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("New Incoming Connection from client : ", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.reedLoop(ws)
+
+}
 func main() {
-	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	app.Listen(":3000")
 }
